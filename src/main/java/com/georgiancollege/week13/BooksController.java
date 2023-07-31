a32cffe923bf8533d762ea3bd4ba82de93df6416
Priyansh Thakar <priyanshthakar@gmail.com>
2023-07-31 11:38:31 -0400

    Final

--- src/main/java/com/georgiancollege/week13/BooksController.java
+++ src/main/java/com/georgiancollege/week13/BooksController.java
@@ -1,12 +1,17 @@
 package com.georgiancollege.week13;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class BooksController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BooksController implements Initializable {
 
     @FXML
     private Label booksLabel;
@@ -35,4 +40,25 @@ public class BooksController {
     @FXML
     private TextArea descriptionTextArea;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ApiResponse apiResponse = Utility.getDataFromApi();
+        System.out.println(apiResponse);
+
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        authorColumn.setCellValueFactory(new PropertyValueFactory<>("author"));
+        genreColumn.setCellValueFactory(new PropertyValueFactory<>("genre"));
+        publishedDateColumn.setCellValueFactory(new PropertyValueFactory<>("publishedDate"));
+        publisherColumn.setCellValueFactory(new PropertyValueFactory<>("publisher"));
+
+        tableView.getItems().addAll(apiResponse.getBookData());
+
+        tableView.getSelectionModel().selectedItemProperty().addListener(
+                (observableValue, oldBookData, newBookData) -> {
+                    booksLabel.setText(newBookData.getTitle());
+                    descriptionTextArea.setText(newBookData.getDescription());
+                }
+        );
+    }
 }
--- src/main/java/com/georgiancollege/week13/Utility.java
+++ src/main/java/com/georgiancollege/week13/Utility.java
@@ -1,5 +1,27 @@
 package com.georgiancollege.week13;
 
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Utility {
-    
+    public static ApiResponse getDataFromApi(){
+        String url = "https://lamp.computerstudi.es/~PriyanshPriyansh01/java/week13/books.json";
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(url)).build();
+
+        try {
+            HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            Gson gson = new Gson();
+            return gson.fromJson(response.body(), ApiResponse.class);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
